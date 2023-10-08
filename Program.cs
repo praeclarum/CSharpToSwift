ce3175ad976d062c7b651a30df234abd2fc9946f
Frank A. Krueger <fak@praeclarum.org>
2023-10-08 10:01:38 -0700

    Initial commit based on Armina fbac894f

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using CSharpToSwift;
+
+Console.WriteLine("C# to Swift Transpiler");
+
+string projectFilePath = "/Users/fak/Dropbox/Projects/Circuit/CircuitGalleryLib/CircuitLib.csproj";
+
+try {
+    var transpiler = new Transpiler(projectFilePath);
+    await transpiler.TranspileAsync();
+}
+catch (Exception ex) {
+    Console.WriteLine(ex.Message);
+}
+
